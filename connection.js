309163006f2674296102c90484c2133ac79b9867
SaKu <sakuofficial@gmail.com>
2021-05-14 00:21:33 +0530

    Initial Commit

---
+++ connection.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBOfNZHQ9c_-KS6WDdGMjKSRvvGNzAFNg4",
+    authDomain: "smart-power-meter-44e02.firebaseapp.com",
+    databaseURL: "https://smart-power-meter-44e02-default-rtdb.firebaseio.com",
+    projectId: "smart-power-meter-44e02",
+    storageBucket: "smart-power-meter-44e02.appspot.com",
+    messagingSenderId: "644579599926",
+    appId: "1:644579599926:web:4a93acae4245a03fcc9a97",
+    measurementId: "G-K930REWS3E"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
---
+++ sidebar.php
@@ -0,0 +1,31 @@
+<nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          <li class="nav-item menu-open">
+            <a href="index.php" class="nav-link active">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Dashboard
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="customers.php" class="nav-link">
+              <i class="nav-icon fas fa-user"></i>
+              <p>
+                Customers
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="bills.php" class="nav-link">
+              <i class="nav-icon fas fa-user"></i>
+              <p>
+                Bills
+              </p>
+            </a>
+          </li>
+        </ul>
+      </nav>
